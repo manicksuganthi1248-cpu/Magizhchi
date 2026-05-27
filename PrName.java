92204cde136c9580fd4f48c3c85391d3dea9ab92
unknown <you@example.com>
2026-05-27 17:52:31 +0300

    successful

---
+++ PrName.java
@@ -0,0 +1,5 @@
+class PrName{
+	public static void main(String [] args){
+		System.out.println("Manick");
+	}
+}	
---
+++ SMart.java
@@ -0,0 +1,10 @@
+class SMart{
+	public static void main(String [] args){
+		System.out.println("*****Super Market*****");
+		System.out.println("1 Fruits 100");
+		System.out.println("2 Milk   30");
+		System.out.println("3 Icecream 80");
+		System.out.println("***************");
+	}
+}	
+	
