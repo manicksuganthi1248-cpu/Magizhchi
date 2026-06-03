652288f5c6298d856cac17ae033065080921e993
unknown <you@example.com>
2026-06-03 06:38:00 +0300

    3 june homework

---
+++ SimpleCalc.java
@@ -0,0 +1,15 @@
+class SimpleCalc{
+	public static void main(String [] args){
+		int a = 110;
+		int b = 12;
+		System.out.println("NUM 1 : "+a);
+		System.out.println("NUM 2 : "+b);
+		System.out.println("ADDITION = "+(a+b));
+		System.out.println("SUBRACTION = "+(a-b));
+		System.out.println("MULTIPLICATION = "+(a*b));
+		System.out.println("DIVISION = "+(a/b));
+	}
+}	
+		
+		
+		
