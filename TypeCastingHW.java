60c0e7a8d790ebcfe6fc46859699e1efc47953d3
unknown <you@example.com>
2026-06-13 07:31:16 +0300

    TYPE CASTING HOMEWORK

---
+++ TypeCastingHW.java
@@ -0,0 +1,42 @@
+class TypeCastingHW{
+	public static void main (String args []){
+		
+		int e=12;
+		double d=e;
+		System.out.println(d);
+		
+		double a=12;
+		int b =(int)a;
+		System.out.println(b);
+		
+		char s='B';
+		int ascii=s;
+		System.out.println("The ASCII Value: " + s + "is" + ascii);
+		
+		int k=13;
+		byte l=(byte) k;
+		System.out.println(l);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
